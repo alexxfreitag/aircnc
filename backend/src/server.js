8e5cffa5a22c376e7fca2108f4420047468fb208
Alex Freitag <alex.f@quantich.com.br>
2019-10-05 16:54:44 -0300

    finalização do cadastro de spot e do site como um todo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -29,6 +30,7 @@ não vai conseguir ler em json
 app.use(cors()); 
 //para dizer ao express que irá receber informações em json
 app.use(express.json()); 
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
